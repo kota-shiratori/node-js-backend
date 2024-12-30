0024f2bfcd23b56bea0dc78a1c449e143926e248
kota_shiratori <swan.kouta@gmail.com>
2024-12-30 11:54:56 +0900

    ✨Express

--- app.mjs
+++ app.mjs
@@ -1,8 +1,38 @@
 import * as http from "http"; // node.js 標準で装備しているhttp(簡易的なサーバーを立ち上げる)
+import express from "express";
 
-const server = http.createServer(function (req, res) {
-    console.log(req.url);
-    res.end("hello");
+const PORT = 8080;
+const app = express(PORT);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send(`
+        <a href="/result?param1=1">Get Method Link</a>
+        <form action="/result" method="POST">
+        <input type="text" name="title">
+        </form>`)
+})
+
+app.get("/result", (req, res) => {
+    const params = req.query;
+    console.log(params);
+    res.send({ message: "hello" });
+})
+
+app.post("/result", (req, res) => {
+    const params = req.body;
+    console.log(params);
+    res.send(`get end`);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Start: http://localhost:${PORT}`);
 });
 
-server.listen(8080);
+// const server = http.createServer(function (req, res) {
+//     console.log(req.url);
+//     res.end("hello");
+// });
+
+// server.listen(8080);
